6c8d93e0c5b9b3d64db3996b964ac3e2585ed900
achen <achenspring@163.com>
2017-07-12 11:21:35 +0800

    commit
    commit

--- src/main/java/com/paris/backend/DemoApplication.java
+++ src/main/java/com/paris/backend/DemoApplication.java
@@ -1,17 +1,12 @@
 package com.paris.backend;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class DemoApplication
-{
-	public static void main(String[] args)
-	{
+public class DemoApplication {
+
+	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
